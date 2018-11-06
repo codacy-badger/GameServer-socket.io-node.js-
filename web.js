ea673a674af84496a8bd8e2a41586c9b38a073ff
gaebhi <kdkim89@gmail.com>
2018-11-06 23:26:46 +0900

    1

--- web.js
+++ web.js
@@ -1,6 +1,24 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var multer = require('multer');
+var path = require('path');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var uploads = require('./routes/upload');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/upload',express.static('upload'));
+
+app.use('/upload',express.static('upload'));
+app.use('/', index);
+app.use('/users', users);
+app.use('/upload', uploads);
+
 var port = normalizePort(process.env.PORT || '8002');
 
 function normalizePort(val) {
@@ -20,7 +38,6 @@ function normalizePort(val) {
 }
 
 server.listen(port);
-// WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
