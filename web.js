df5b8571bcb4f5bd8aae86ad3d1044e20d3fbf0c
gaebhi <kdkim89@gmail.com>
2019-02-22 22:20:20 +0900

    init

--- web.js
+++ web.js
@@ -1,51 +1,69 @@
-var express = require('express');
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var multer = require('multer');
+console.log('-----start server-----\r\n');
+
+var fs = require('fs');
+var util = require('util');
 var path = require('path');
+var async = require('async');
+var http = require('http');
+var express = require('express');
+var app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var uploads = require('./routes/upload');
+app.get('/index.html', function(req,res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/upload',express.static('upload'));
-
-//app.use('/', index);
-app.use('/users', users);
-app.use('/upload', uploads);
-
-var port = normalizePort(process.env.PORT || '8002');
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
+function Print(str)
+{
+  console.log(str);
 }
 
-server.listen(port);
+function TimeStamp()
+{
+  var dt = new Date();
+  return util.format('[%d-%d-%d_%d:%d:%d.%d]', dt.getYear()+1900, dt.getMonth()+1, dt.getDate(), dt.getHours(), dt.getMinutes(), dt.getSeconds(), dt.getMilliseconds());
+}
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+var serverInfo = null;
+function PrintServerInfo(config)
+{
+  serverInfo =  'ServerIP  : ' + config.ServerIP + '\r\n';
+  serverInfo += 'TimeStamp : ' + TimeStamp() + '\r\n';
+  serverInfo += 'Version   : ' + config.Version + '\r\n';
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+  Print(serverInfo);
+}
+
+var server = null;
+var io = null;
+var router = null;
+
+function Main()
+{
+  var defaultConfigFile = 'ServerConfig.json';
+  var info = fs.readFileSync(defaultConfigFile,'utf8');
+  var config = JSON.parse(info);
+
+  PrintServerInfo(config);
+
+  async.series
+  (
+    [
+      function(callback)
+      {
+        server = http.createServer(app);//express사용하여 http 서버 생성
+        io = require('socket.io')(server);//생성된 http 서버를 socket.io 서버로 업그레이드
+        server.listen(config.ServerPort,function(){console.log('listening on :' + config.ServerPort);});
+        router = require('socket.io-events')();
+        io.use(router);
+        callback();
+      },
+    ],
+    function(err,results)
+    {
+      process.on('uncaughtException', function(err){console.log(err.stack);});
+      console.log('-----start success-----\r\n');
+    }
+  )
+}
+
+Main();
